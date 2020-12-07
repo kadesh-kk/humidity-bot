235b6e0910dc62e6392d30823dc7ee4a8a2f202a
kadesh-kk <75239923+kadesh-kk@users.noreply.github.com>
2020-12-07 23:39:18 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 from humidity.humidity import humidity_calc
 import datetime
 import os
+import pickle
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -22,10 +23,28 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-@app.route("/")
+@app.route("/", methods = "GET")
 def hello_world():
-    dt_now = datetime.datetime.now()
-    return "hello world!\n今は{0}年{1}月{2}日 {3}時".format(dt_now.year, dt_now.month, dt_now.day, dt_now.hour)
+    dt_now = datetime.datetime.now() + datetime.timedelta(days = 1)
+
+    pref_name = "東京都"
+    city_name = "八王子市"
+    temperature = 13
+    humidity_rate = 25
+    predict_humidity = humidity_calc(pref_name, city_name, int(temperature), float(humidity_rate))
+    return "hello world!\n今は{0}年{1}月{2}日 {3}時です!\n東京都八王子市の湿度は{4}%だよ!".format(dt_now.year, dt_now.month, dt_now.day, dt_now.hour, predict_humidity)
+
+def get_json_data():
+    # 市区町村のデータを取得
+    with open("./pkl/forecast_url.pickle", mode='rb') as f:
+        pref_city_dict = pickle.load(f)
+    return pref_city_dict
+
+def top_page():
+    pref_city_dict = get_json_data()
+    # modelを書く
+
+    # return render_template("index.html")
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -48,7 +67,7 @@ def callback():
 def handle_message(event):
     try:
         receive_message =  event.message.text
-        city_name, temperature, humidity_rate = receive_message.split("\n")
+        pref_name, city_name, temperature, humidity_rate = receive_message.split("\n")
 
         # 文字列の置換
         for rep in ["度", "℃", "ど"]:
@@ -56,14 +75,14 @@ def handle_message(event):
         for rep in ["％", "%", "ぱーせんと", "パーセント", "percent"]:
             humidity_rate = humidity_rate.replace(rep, "")
 
-        predict_humidity = humidity_calc(city_name, int(temperature), float(humidity_rate))
+        predict_humidity = humidity_calc(pref_name, city_name, int(temperature), float(humidity_rate))
         if predict_humidity >= 100:
             send_message = "結露ができそうだよ"
         else:
             send_message = "湿度は{0}%になりそうだよ".format(predict_humidity)
 
     except:
-        send_message = "市町村名\n温度\n湿度\nで送信してください。\n\n---例---\n新宿区\n25℃\n70％\n"
+        send_message = "都道府県名\n市町村名\n温度\n湿度\nで送信してください。\n\n---例---\n東京都\n新宿区\n25℃\n70％\n"
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=send_message))
